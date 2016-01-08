bf62a56418abe276fa9bfff8ec1ac7ec0e2d51f6
Erik Donohoo <erikdonohoo@gmail.com>
2016-01-08 15:27:28 -0700

    bullet proofing cli tool

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ var errors = {
     network: 'Cound not communicate with Comforter instance',
     missingCoverage: 'Missing path to lcov file or generated coverage percentange',
     badRequest: 'Coverage data was sent incorrectly or not accepted by Comforter',
-    lcovFile: 'LCOV file could not be found'
+    lcovFile: 'LCOV file could not be found',
+    missingRequiredParams: 'Missing one or many of required params (commit, branch project)'
 };
 
 // begin
 console.log('Posting coverage to Comforter...');
-
-// post to ci, with status running
 
 // check passed params
 handleParams(argv);
@@ -46,31 +45,37 @@ function sendRequest () {
         } catch (err) {
             console.error(chalk.red('ERROR') + ' ' + errors.lcovFile);
             console.error(err);
-            fail(errors.lcovFile);
+            fail();
         }
     }
 
-    restler.post('http://localhost:3000/api/apps/1/coverage', {
+    restler.post('http://localhost:3000/api/apps/' + argv.project + '/coverage', {
         multipart: true,
         data: data
+    }).on('success', function (data) {
+        console.log('Comforter Coverage: ' + chalk.green(data.coverage));
     }).on('error', function (err) {
         console.error(chalk.red('ERROR') + ' ' + errors.network);
         console.error(err);
-        fail(errors.network);
+        fail();
     }).on('fail', function (data) {
         console.error(chalk.red('FAILED') + ' ' + errors.badRequest);
         console.error(data);
-        fail(errors.badRequest);
+        fail();
     });
 }
 
-function fail (reason) {
-    process.exit(reason);
+function fail () {
+    process.exit(1);
 }
 
 function handleParams (params) {
     if (!params.path && !params.coverage) {
         console.error(chalk.red('ERROR') + ' ' + errors.missingCoverage);
-        fail(errors.missingCoverage);
+        fail();
+    }
+    if (!params.project || !params.branch || !params.commit) {
+        console.error(chalk.red('ERROR') + ' ' + errors.missingRequiredParams);
+        fail();
     }
 }
