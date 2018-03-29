0d816e6193a2a08606713a70066697facdcafa1b
Erik Donohoo <erikdonohoo@gmail.com>
2018-03-29 10:47:06 -0600

    add required name param

--- test/setup.js
+++ test/setup.js
@@ -26,7 +26,8 @@ lab.experiment('setup: params', function () {
 		branch: 'my-branch',
 		commit: 'sha',
 		project: 1,
-		apiKey: 'key'
+		apiKey: 'key',
+		name: 'stuff'
 	};
 
 	var keys = Object.keys(params);
@@ -46,7 +47,7 @@ lab.experiment('setup: params', function () {
 		lab.test('fails when missing required params combo ' + index, function (done) {
 			child.exec('node ' + __dirname + '/../lib/index.js --coverage 98.8 ' + string + ' > ' + outfile, function (err) {
 				code.expect(err).to.exist();
-				code.expect(err.toString()).to.contain('Missing one or many of required params (commit, branch, project, host, apiKey)');
+				code.expect(err.toString()).to.contain('Missing one or many of required params (commit, branch, project, host, apiKey, name)');
 				done();
 			});
 		});
